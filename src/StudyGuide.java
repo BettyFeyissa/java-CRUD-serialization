8bd210b295b901bba6cfc3a3a5c35e6af09d4858
betelhem feyissa <betelhemfeyissa@betelhems-Air.lan>
2022-11-06 23:54:33 -0600

    Added loadData method to serialize and deserialize

--- src/StudyGuide.java
+++ src/StudyGuide.java
@@ -126,4 +126,55 @@ public class StudyGuide {
 
     }
 
+    public static void loadData(){
+// we need to read from the file object.ser the data for our topic
+// and if possible create a new employee otherwise return null
+
+        ArrayList <Topic> topics = new ArrayList<>(); // this create an object of type topic to receive data from file or return
+
+        //Deserialization
+        try {
+            // read object from a file
+            FileInputStream file = new FileInputStream("topics.ser");
+            // create a connect to a file
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            // method for deserialization for an object
+            topics = (ArrayList<Topic>) in.readObject();
+            // ^ read object and convert data to type Employee
+
+            in.close();
+            file.close();
+
+            System.out.println("Object has been deserialized");
+            System.out.println(topics.size());
+
+        } catch (IOException i){
+            i.printStackTrace();
+
+        }catch (ClassNotFoundException c){
+            c.printStackTrace();
+        }
+
+        // Serialization
+        try{
+            FileOutputStream fileOut = new FileOutputStream("topics.ser");
+            // ^ opening a connect to a new file and allowing to connect
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            // ^ streaming data from an object into a file
+            out.writeObject(topics);
+            // take this object and i'm lobbing it
+            out.close();
+            // close it once we are done with the file
+            fileOut.close();
+            // close it once we are done with the file
+            System.out.println("Serialized data is saved!");
+
+        }catch (IOException i) {
+            i.printStackTrace();
+            // history of all the methods that were called - allows us to see where the code went wrong.
+        }
+
+    }
+
 }
